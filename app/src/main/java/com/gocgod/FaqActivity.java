17bac83d933f3eb02b675ee3e6618654f94d7aa7
Aurelia Rianto <aurelia.rianto@gmail.com>
2016-08-24 10:34:21 +0700

    howtobuy

--- app/src/main/java/com/gocgod/FaqActivity.java
+++ app/src/main/java/com/gocgod/FaqActivity.java
@@ -28,7 +28,7 @@ public class FaqActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            finish();
+
         }
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/com/gocgod/HowToBuySubscriberActivity.java
@@ -0,0 +1,13 @@
+package com.gocgod;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HowToBuySubscriberActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_buy_subscriber);
+    }
+}
